520f827a9aa5fe0b725c266b93366d2c0545db9c
tateki.obori <tateki.obori@cct-inc.co.jp>
2022-07-11 09:22:37 +0900

    課題2完了

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,8 +1,8 @@
 export interface Item {
-  id?: number;
+  id: number;
   date?: string ;
-  category?: string;
-  content?: string;
+  category: string;
+  content: string;
   fee?: string;
-  inOrOut?: string
+  inOrOut: string
 }
